93ac449df8d867b58756d84496bfaaee9357c3ba
Harshad Sharma <harshad@sharma.io>
2020-03-14 23:24:07 +0530

    Add endpoint to send frames via HTTP

---
+++ src/zencelium/token_auth.py
@@ -0,0 +1,33 @@
+from functools import wraps
+
+from quart import g
+from quart import request
+from quart.exceptions import Unauthorized
+
+from .models import Agent
+
+class AgentTokenAuth(object):
+    def __init__(self, app):
+        self.app = app
+        self.app.before_request(self.authenticate)
+
+    async def authenticate(self):
+        if 'Authorization' in request.headers:
+            token = request.headers.get('Authorization').replace('Bearer ', '')
+            try:
+                agent = Agent.get_or_none(token=token)
+                if agent:
+                    g.agent = agent
+            except:
+                raise Unauthorized()
+
+    def login_required(self, func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            if not getattr(g, 'agent', None):
+                raise Unauthorized()
+            else:
+                agent = g.agent
+                return await func(agent=agent, *args, **kwargs)
+
+        return wrapper
--- src/zencelium/util.py
+++ src/zencelium/util.py
@@ -14,3 +14,12 @@ def add_space_to_meta(frame, space_name, space_uuid):
         frame._meta.update(space_meta)
     else:
         frame._meta = space_meta
+
+def clean_space_names(self, space_names):
+    if not space_names:
+        return set()
+    if isinstance(space_names, str):
+        space_names = {s.strip() for s in space_names.split(',')}
+    else:
+        space_names = set(space_names)
+    return space_names
